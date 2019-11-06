430e7e48b250847a327d0486d7967e2fd8bf2400
ballstodawalz <lukewalz1@gmail.com>
2019-11-06 17:49:12 -0600

    Container

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,14 @@ class Home extends Component {
     return (
       <div className="container">
         <div className="jumbotron jumbotron-fluid">
+<div className="container">
           <h1 className="display-4">Paws Compliance</h1>
           <p className="lead">
             {" "}
             The foremost compliance experts and essential reminder; creating
             rewarding habits leading to good health and good fortune for all
           </p>
+</div>
         </div>
       </div>
     );
