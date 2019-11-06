ae4f8eb5e9fc201449917572d90a35fcc2018311
ballstodawalz <lukewalz1@gmail.com>
2019-11-06 17:56:54 -0600

    Nav

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ class App extends Component {
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+   
             <ul className="navbar-nav">
               <li>
                 <Link to={"/"} className="nav-link">
@@ -37,6 +39,7 @@ class App extends Component {
                 </Link>
               </li>
             </ul>
+<\div>
           </nav>
           <hr />
           <Switch>
