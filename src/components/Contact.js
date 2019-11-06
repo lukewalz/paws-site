04968876655fd2e2f611cf083b0cfa70aa7a2eb6
ballstodawalz <lukewalz1@gmail.com>
2019-11-06 16:35:54 -0600

    form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,29 @@ class Contact extends Component {
   render() {
     return (
       <div className="container">
-        <h2>Contact</h2>
+        <h2>Contact for more information</h2>
+        <form name="contact" method="POST" data-netlify="true">
+          <div className="form-group">
+            <label for="name">Your Name:</label>
+            <input type="text" class="form-control" name="name" id="name" />
+          </div>
+          <div className="form-group">
+            <label for="email">Your Email:</label>
+            <input type="email" name="email" class="form-control" id="email" />
+          </div>
+          <div className="form-group">
+            <label for="message">Message: </label>{" "}
+            <textarea
+              class="form-control"
+              name="message"
+              id="message"
+              rows="3"
+            ></textarea>
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Send
+          </button>
+        </form>
       </div>
     );
   }
