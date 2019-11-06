70188e630bc0528278e81fdd55d3cc66c00df16b
ballstodawalz <lukewalz1@gmail.com>
2019-11-06 16:25:01 -0600

    stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import React from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          PAWS Compliance
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container-fluid">
+          <nav className="navbar navbar-expand-lg navbar-light justify-content-between">
+            <img
+              alt="logo"
+              src={require("./logo.png")}
+              className="navbar-brand image navbar-left"
+            />
+            <ul className="navbar-nav">
+              <li>
+                <Link to={"/"} className="nav-link">
+                  {" "}
+                  App{" "}
+                </Link>
+              </li>
+              <li>
+                <Link to={"/about"} className="nav-link">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to={"/contact"} className="nav-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <hr />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+// Home.js
+
+import { MdAndroid } from "react-icons/md";
+
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron jumbotron-fluid">
+          <h1 className="display-4">Paws Compliance</h1>
+          <p className="lead">
+            {" "}
+            The foremost compliance experts and essential reminder; creating
+            rewarding habits leading to good health and good fortune for all
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
