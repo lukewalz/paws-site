60123ae2c426e39a9c521caf4c2e12cf93007422
ballstodawalz <lukewalz1@gmail.com>
2020-03-20 01:09:33 -0500

    more

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { MdAndroid } from "react-icons/md";
 import { } from '../../src/'
 import React, { Component } from "react";
 import { FaApple, FaAndroid } from 'react-icons/fa';
+import 'lazysizes';
 
 var imgUrl = require('../assets/Dog_getting_Treat_v2.png')
 
@@ -32,15 +33,15 @@ class Home extends Component {
               for your pets.</p>
           </div>
           <div className='col-lg-6 col-md-6 col-sm-12'>
-            <img style={{
+            <img alt='post it notes' className='lazyload' style={{
               maxWidth: '100%'
-            }} src={require('../assets/post-it-notes.jpg')} />
+            }} data-src={require('../assets/post-it-notes.jpg')} />
           </div>
           <div className="w-100" style={{ height: '100px' }}></div>
           <div className='col-lg-6 col-md-6 col-sm-12'>
-            <img style={{
+            <img className='lazyload' alt='dog' style={{
               maxWidth: '100%'
-            }} src={require('../assets/Dog_and_Phone.png')} />
+            }} data-src={require('../assets/Dog_and_Phone.png')} />
           </div>
           <div className='col-lg-6 col-md-6 col-sm-12'>
             <p className='subtitleText' style={{ color: 'black' }}>Remembering Made Foolproof</p>
@@ -55,9 +56,9 @@ class Home extends Component {
 
         </div>
         <div className='row overlayRow' style={{ backgroundColor: 'gray', justifyContent: 'center', display: 'flex' }}>
-          <img className='col-lg-6 col-md-12 col-sm-12' style={{
+          <img className='lazyload col-lg-6 col-md-12 col-sm-12' style={{
             maxHeight: '380px'
-          }} src={require('../assets/Phone_Screenshots.png')} />
+          }} alt='phone screenshots' data-src={require('../assets/Phone_Screenshots.png')} />
           <div className='col-lg-6 col-md-12 col-sm-12'>
             <p className='subtitleText' >Healthy Habits are Rewarding...
               and Contagious</p>
@@ -71,21 +72,21 @@ class Home extends Component {
         </div>
         <div className='row'>
           <div className='col-lg-5 col-md-4 col-sm-12' style={{ textAlign: 'center' }}>
-            <img style={{
+            <img className='lazyload' style={{
               maxHeight: '200px'
-            }} src={require('../assets/Dog_Phone_Logo.png')} />
+            }} alt='dog' data-src={require('../assets/Dog_Phone_Logo.png')} />
             <p>Remind.</p>
           </div>
           <div className='col-lg-2 col-md-4 col-sm-12' style={{ textAlign: 'center' }}>
-            <img style={{
+            <img alt='cat treat' className='lazyload' style={{
               maxHeight: '200px'
-            }} src={require('../assets/Cat_Treat.jpg')} />
+            }} data-src={require('../assets/Cat_Treat.jpg')} />
             <p>Administer.</p>
           </div>
           <div className='col-lg-5 col-md-4 col-sm-12' style={{ textAlign: 'center' }}>
-            <img style={{
+            <img alt='man kissing dog' className='lazyload' style={{
               maxHeight: '200px'
-            }} src={require('../assets/Dog_kiss.jpg')} />
+            }} data-src={require('../assets/Dog_kiss.jpg')} />
             <p>Rewards.</p>
           </div>
           <div style={{ width: '100%', height: '70px' }}></div>
@@ -94,48 +95,48 @@ class Home extends Component {
           </div>
           <div className='col-lg-4 col-md-4 col-sm-12' style={{ textAlign: 'center', flexDirection: 'row' }}>
 
-            <p className='text' style={{ color: 'black', textAlign: 'center' }}>            <img style={{
+            <p className='text' style={{ color: 'black', textAlign: 'center' }}>            <img alt='red checkmark' className='lazyload' style={{
               maxHeight: '50px'
-            }} src={require('../assets/Red_Check_Gray_Circle.png')} />Paws ensures the correct preventative is being given to the correct pet.</p>
+            }} data-src={require('../assets/Red_Check_Gray_Circle.png')} />Paws ensures the correct preventative is being given to the correct pet.</p>
           </div>
           <div className='col-lg-4 col-md-4 col-sm-12' style={{ textAlign: 'center', flexDirection: 'row' }}>
 
-            <p className='text' style={{ color: 'black', textAlign: 'center' }}>            <img style={{
+            <p className='text' style={{ color: 'black', textAlign: 'center' }}>            <img alt='red checkmark' className='lazyload' style={{
               maxHeight: '50px'
-            }} src={require('../assets/Red_Check_Gray_Circle.png')} />Keeps track of your inventory. Paws will alert you of expiration dates and when running low.</p>
+            }} data-src={require('../assets/Red_Check_Gray_Circle.png')} />Keeps track of your inventory. Paws will alert you of expiration dates and when running low.</p>
           </div>
           <div className='col-lg-4 col-md-4 col-sm-12' style={{ textAlign: 'center', flexDirection: 'row' }}>
 
-            <p className='text' style={{ color: 'black', textAlign: 'center' }}>            <img style={{
+            <p className='text' style={{ color: 'black', textAlign: 'center' }}>            <img alt='red checkmark' className='lazyload' style={{
               maxHeight: '50px'
-            }} src={require('../assets/Red_Check_Gray_Circle.png')} />Paws clears any confusion over dosing (monthly? with food?) and which preventatives are for which parasites.</p>
+            }} data-src={require('../assets/Red_Check_Gray_Circle.png')} />Paws clears any confusion over dosing (monthly? with food?) and which preventatives are for which parasites.</p>
           </div>
         </div>
         <div className='row overlayRow' style={{ backgroundColor: 'maroon' }}>
           <div className='col-lg-5 col-md-5 col-sm-12'>
             <p className='subtitleText'>Rest easy. <br></br>Paws has you covered</p>
-            <p className='text'>            <img style={{
+            <p className='text'>            <img className='lazyload' alt='red checkmark' style={{
               maxHeight: '25px'
-            }} src={require('../assets/Red_Check.png')} />Preventatives given on time</p>
-            <p className='text'><img style={{
+            }} data-src={require('../assets/Red_Check.png')} />Preventatives given on time</p>
+            <p className='text'><img alt='red checkmark' className='lazyload' style={{
               maxHeight: '25px'
-            }} src={require('../assets/Red_Check.png')} />Dosing directions and history</p>
-            <p className='text'><img style={{
+            }} data-src={require('../assets/Red_Check.png')} />Dosing directions and history</p>
+            <p className='text'><img alt='red checkmark' className='lazyload' style={{
               maxHeight: '25px'
-            }} src={require('../assets/Red_Check.png')} />Inventory management</p>
+            }} data-src={require('../assets/Red_Check.png')} />Inventory management</p>
 
             <button type="button" className="btn btn-danger">DOWNLOAD<FaApple /><FaAndroid /></button>
           </div>
           <div className='col-lg-7 col-md-7 col-sm-12'>
-            <img style={{
+            <img alt='Cat asleep in bed' className='lazyload' style={{
               height: '100%', width: '100%', display: 'block', margin: '20px'
-            }} src={require('../assets/Cat_sleep_bed.jpg')} />
+            }} data-src={require('../assets/Cat_sleep_bed.jpg')} />
           </div>
 
         </div>
         <div className='row' style={{ marginTop: '12em', marginBottom: '12em' }}>
           <div className='col-lg-6 col-md-6 col-sm-12' style={{ alignContent: 'center', alignItems: 'center', display: 'flex', justifyContent: 'space-around' }}>
-            <img src={require("../assets/Get_your_paws_on_it.png")} style={{ maxHeight: 200 }} />
+            <img alt='PAWS Logo' className='lazyload' data-src={require("../assets/Get_your_paws_on_it.png")} style={{ maxHeight: 200 }} />
 
           </div>
           <div className='col-lg-6 col-md-6 col-sm-12' style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', alignContent: 'center' }}>
